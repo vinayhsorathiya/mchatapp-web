b5735d1dbe395746f9de44b8f3be2e05e9956b11
vinayhsorathiya <vinayhsorathiya@users.noreply.github.com>
2018-04-03 22:30:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -15,16 +15,8 @@ restService.use(bodyParser.json());
 
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
   return res.json({
-    speech: "vinay",
-    displayText: "sample",
-    source: "webhook-echo-sample"
+    displayText: "sample"
   });
 });
 
